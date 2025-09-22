6b082c97921f3819031db5df57337d1b87246b69
Lucas Heinemann <70389444+SuperUltraLucas@users.noreply.github.com>
2025-09-22 21:40:49 +0200

    use dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Moebelbau/',
-  plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
+  plugins: [react()]
 })
